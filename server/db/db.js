7d8d27676134e95a8a1b10419bf9a2ca1a8a3f72
Eunji Song <eunji.l.song@gmail.com>
2018-06-13 12:38:40 -0400

    heroku

--- server/db/db.js
+++ server/db/db.js
@@ -3,4 +3,4 @@ const Sequelize = require('sequelize');
 const db = new Sequelize(process.env.DATABASE_URL || 'postgres://localhost:5432/remember', {
   logging: false,
 })
-module.exports = db;
+module.exports = db
